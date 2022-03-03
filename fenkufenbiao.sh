98dbd6f9d71c0f911332701bd443664d69cdad63
Chting <654852835@qq.com>
2022-03-03 11:12:11 +0800

    Create fenkufenbiao.sh

---
+++ fenkufenbiao.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+#分库分表执行SQL
+#CMD="mysql -P3306 -hxxxxx -uxxxx -pxxxx"
+#CMD="mysql -P18741 -h172.17.241.247 -uroot -pxxxx"
+DB="orders_"
+Table="zk_tbk_order_"
+for ((t=0;t<=15;t++))
+do
+for ((i=0;i<=127;i++))
+do
+     echo ${DB}${t}.${Table}${i}
+        $CMD -e " ALTER TABLE ${DB}${t}.${Table}${i} add index s_idx_adzone_id_create_time (adzone_id,create_time) "
+        $CMD -e " ALTER TABLE ${DB}${t}.${Table}${i} add index s_idx_user_id (user_id) "
+        $CMD -e " ALTER TABLE ${DB}${t}.${Table}${i} add index s_idx_device_id (device_id) "
+done
+done
